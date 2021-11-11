f63adb198aba811deeb56cadea114a41bbebc747
Matteusns <mateusnsousa77@gmail.com>
2021-11-11 16:06:11 -0300

    Finalização projeto 1

--- Projeto1/scripts.js
+++ Projeto1/scripts.js
@@ -1,5 +1,6 @@
 var products = [
     {
+        "id" : 0,
         "photo": "img/big-mac.png",
         "name": "Big Mac",
         "price": 5.99,
@@ -7,6 +8,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 1,
         "photo": "img/mc-chicken.png",
         "name": "Mc Chicken",
         "price": 4.99,
@@ -14,6 +16,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 2,
         "photo": "img/double-cb.png",
         "name": "Double Cheese Burger",
         "price": 2.99,
@@ -21,6 +24,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 3,
         "photo": "img/fries.png",
         "name": "Batata frita",
         "price": 2.99,
@@ -28,6 +32,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 4,
         "photo": "img/nuggets.png",
         "name": "Mc Nuggets",
         "price": 3.49,
@@ -35,6 +40,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 5,
         "photo": "img/salad.png",
         "name": "Salada",
         "price": 2.79,
@@ -42,6 +48,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 6,
         "photo": "img/cola.png",
         "name": "Coca Cola",
         "price": 1.99,
@@ -49,6 +56,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 7,
         "photo": "img/lipton.png",
         "name": "Ice Tea",
         "price": 1.99,
@@ -56,6 +64,7 @@ var products = [
         "quantity": 1
     },
     {
+        "id" : 8,
         "photo": "img/water.png",
         "name": "Água",
         "price": 1.49,
@@ -64,3 +73,31 @@ var products = [
     }
 ];
  
+const SelfServiceMachine = {
+    data() {
+        return {
+            products: window.products
+        }
+    },
+    methods: {
+        total: function (){
+            var total = 0
+            this.products.forEach(({quantity, price, active}) => {
+                if (active) {
+                    total += price * quantity
+                }
+            })
+            return total.toFixed(2);
+        },
+        toggleSelecionar : function (index) {
+            this.products[index].active = !this.products[index].active
+            if (!this.products[index].active) {
+                this.products[index].quantity = 1
+            }
+        }
+    }
+}
+
+Vue.createApp(SelfServiceMachine).mount('#app');
+
+
