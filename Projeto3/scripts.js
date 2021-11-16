a81cdfda26e5f63788dca1648232a208d38ad763
Matteusns <mateusnsousa77@gmail.com>
2021-11-16 13:37:39 -0300

    Finalização projeto 3 (To Do)

--- Projeto3/scripts.js
+++ Projeto3/scripts.js
@@ -1,9 +1,30 @@
-
-
 const todosApp = {
     data() {
         return {
-            todoList: []
+            todoList : [],
+            newTask : ""
         }
+    },
+    methods: {
+        addNewTask : function() {
+            if(this.newTask){
+                this.todoList.push({title : this.newTask})
+                this.newTask = ""
+            }else{
+                alert('Campo não pode estar vazio')
+            }
+        },
+        clearTasks : function() {
+            this.todoList = []
+        }
+    },
+    created () {
+        this.todoList = localStorage.getItem('todoList')?JSON.parse(localStorage.getItem('todoList')): [];
+    },
+    beforeUpdate () {
+        localStorage.setItem('todoList', JSON.stringify(this.todoList))
     }
 }
+
+
+Vue.createApp(todosApp).mount('#app')
