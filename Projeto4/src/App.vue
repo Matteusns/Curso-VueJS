ee2dd7b62eaff503463d82b819c95a1482ef816f
Matteusns <mateusnsousa77@gmail.com>
2021-11-17 10:44:50 -0300

    Finalização Projeto 4 (Quizz)

--- Projeto4/src/App.vue
+++ Projeto4/src/App.vue
@@ -1,50 +1,109 @@
 <template>
   <div>
+    <PlacarPontos :placar="this.placar"/>
 
     <template v-if="this.pergunta">
       <h1 v-html="this.pergunta"> </h1>
 
       <template v-for="(pergunta, index) in this.perguntas" :key="index">
         <input type="radio" name="options"
+          :disabled=this.resEnviada
           :value=pergunta
           :id=pergunta
           v-model="this.resEscolhida" 
         >
+
         <label v-html="pergunta" :for=pergunta ></label> <br />
+
       </template>
 
-      <button class="send" type="button">Send</button>
+      <button class="send" type="button" 
+        @click="this.validarPergunta()"
+        v-if="!this.resEnviada"
+      >
+        Enviar
+      </button>
     </template>
   </div>
+  <section v-if="this.resEnviada">
+    <h4 v-if="this.resEscolhida == this.resCerta">
+      &#9989; Vc acertou.
+    </h4>
+    <h4 v-if="this.resEscolhida !== this.resCerta">
+      &#10060; Vc errou. a resposta correta era "{{this.resCerta}}"
+    </h4>
+    <button class="send"
+      @click="this.getPerguntas()"
+    > Proxima Pergunta</button>
+    
+    <button class="reset" type="button"
+      @click="this.resetPlacar()"
+    >
+      Resetar Placar
+    </button>
+  </section>
 </template>
 
 <script>
+  import PlacarPontos from '@/components/PlacarPontos.vue'
+
 
   export default {
     name: 'App',
+    components: {
+      PlacarPontos
+    },
     data() {
       return {
         pergunta: undefined,
         resIncorretas: undefined,
-        resCertas: undefined,
+        resCerta: undefined,
         resEscolhida: undefined,
+        resEnviada: false,
+        placar: [0,0],
       }
     },
     computed : {
       perguntas() {
         let res = JSON.parse(JSON.stringify(this.resIncorretas));
-        res.splice(Math.random() * res.length, 0 ,this.resCertas);
+        res.splice(Math.random() * res.length, 0 ,this.resCerta);
         return res;
       }
     },
-    created () {
+    methods: {
+      validarPergunta() {
+        if (this.resEscolhida) {
+          this.resEnviada = true
+          if (this.resCerta == this.resEscolhida) {
+            console.log('a')
+            this.placar[0] ++
+          } else {
+            console.log('b')
+            this.placar[1] ++
+          }
+        }else{
+          alert("Escolha uma das opções")
+        }
+      },
+      getPerguntas(){
         this.axios
         .get("https://opentdb.com/api.php?amount=1&category=27&difficulty=easy")
         .then((response) => {
           this.pergunta = response.data.results[0].question
-          this.resCertas = response.data.results[0].correct_answer
+          this.resCerta = response.data.results[0].correct_answer
           this.resIncorretas = response.data.results[0].incorrect_answers
+          this.resEnviada = false
+          this.resEscolhida = undefined
         })
+
+      },
+      resetPlacar() {
+        this.getPerguntas()
+        this.placar = [0,0]
+      }
+    },
+    created () {
+      this.getPerguntas()
     }
   }
 
@@ -64,15 +123,22 @@
       margin: 12px 4px;
     }
 
-    button.send {
-      margin-top: 12px;
+    button {
+      margin: 12px 16px;
       height:40px;
       max-width: 120px;
       padding: 0 16px;
-      color: #ffffff;
-      background-color: #1867c0;
-      border: 1px solid #1867c0;
       cursor: pointer;
+      &.send{
+        color: #ffffff;
+        background-color: #1867c0;
+        border: 1px solid #1867c0;
+      }
+      &.reset {
+        color: #ffffff;
+        background-color: #3faa14;
+        border: 1px solid #3faa14;
+      }
     }
   }
 </style>
---
+++ Projeto4/src/components/PlacarPontos.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="score">
+        Jogador <span>{{placar[0]}}</span> x <span>{{placar[1]}}</span> Computador
+    </section>
+</template>
+<script>
+    export default {
+        props: {
+            placar: Array
+        }
+    }
+</script>
+
+<style lang="scss">
+    section.score {
+        border-bottom: 1px solid #000;
+        padding: 24px;
+        font-size: 18px;
+
+        span {
+            padding: 8px;
+            font-weight: bold;
+            border: 1px solid #000;
+        }
+    }
+</style>
