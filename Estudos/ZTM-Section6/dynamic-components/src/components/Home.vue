2febf7a9747847be24d1b3274ed23f79f06dd3ee
Matteusns <mateusnsousa77@gmail.com>
2022-06-30 13:31:20 -0300

    Finalização aula de components dinamicos

---
+++ Estudos/ZTM-Section6/dynamic-components/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted(){
+    console.log('unmounted');
+  },
+  activated(){
+    console.log('activated')
+  },
+  deactivated(){
+    console.log('deactivated')
+  }
+}
+</script>
+
+<style>
+
+</style>template
